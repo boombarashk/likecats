e9c302d163529027c02cfb3ccb94c3fc5944da0d
boombarashk <boombarashk@yandex.ru>
2018-02-12 22:34:01 +0300

    ie small fix

--- omnomnom/js/main.js
+++ omnomnom/js/main.js
@@ -1,3 +1,25 @@
+;(function(e) {
+    var matches = e.matches || e.matchesSelector || e.webkitMatchesSelector || e.mozMatchesSelector || e.msMatchesSelector || e.oMatchesSelector;
+    !matches ? (e.matches = e.matchesSelector = function matches(selector) {
+        var matches = document.querySelectorAll(selector);
+        var th = this;
+        return Array.prototype.some.call(matches, function(e) {
+            return e === th;
+        });
+    }) : (e.matches = e.matchesSelector = matches);
+})(Element.prototype);
+(function(e){ 
+ e.closest = e.closest || function(css){ 
+   var node = this;
+  
+   while (node) { 
+      if (node.matches(css)) return node; 
+      else node = node.parentElement; 
+   } 
+   return null; 
+ } 
+})(Element.prototype);
+
 function fAddClass(obj, cls) {
   var classes = obj.className ? obj.className.split(' ') : [];
   for (var i = 0; i < classes.length; i++) {
